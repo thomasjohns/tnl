ee49d02ee3f60d8acd854b4c4a855992c263af11
thomasjohns <thomasjohns@fastmail.com>
2021-01-14 00:33:22 -0800

    add --stage argument to cli; add template files to src tree

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,3 @@
-use polars::prelude::*;
 use std::fs::File;
 use structopt::StructOpt;
 
@@ -9,24 +8,52 @@ struct Opts {
 
     #[structopt(value_name = "TABLE_FILE")]
     table_file: String,
+
+    #[structopt(long, value_name = "STAGE", default_value = "exec")]
+    stage: String,
 }
 
-pub fn execute() -> std::result::Result<(), String> {
+pub enum Stage {
+    Lex,
+    Parse,
+    Analyze,
+    Compile,
+    Optimize,
+    Exec,
+}
+
+pub struct Config {
+    pub stage: Stage,
+    pub src_file: File,
+    pub table_file: File,
+}
+
+pub fn execute() -> Result<Config, String> {
     let opts = Opts::from_args();
 
-    let file = match File::open(&opts.table_file) {
+    let src_file = match File::open(&opts.src_file) {
         Ok(res) => res,
-        Err(_) => return Err(format!("could not open file {}", opts.table_file)),
+        Err(_) => return Err(format!("could not open source file '{}'", opts.src_file)),
     };
 
-    // TODO: move to vm
-    let df = CsvReader::new(file)
-        .infer_schema(None)
-        .has_header(true)
-        .finish()
-        .unwrap();
+    let table_file = match File::open(&opts.table_file) {
+        Ok(res) => res,
+        Err(_) => return Err(format!("could not open table file '{}'", opts.table_file)),
+    };
 
-    println!("{:?}", df);
+    let stage = match &opts.stage[..] {
+        "lex" => Stage::Lex,
+        "parse" => Stage::Parse,
+        "analyze" => Stage::Analyze,
+        "compile" => Stage::Compile,
+        "optimze" => Stage::Optimize,
+        "exec" => Stage::Exec,
+        s => return Err(format!("unknown stage '{}'", s)),
+    };
 
-    Ok(())
+    Ok(Config {
+        stage: stage,
+        src_file: src_file,
+        table_file: table_file,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
+mod ast;
 mod cli;
+mod ir;
+mod irgen;
+mod lexer;
+mod optimizer;
+mod parser;
+mod symbol;
+mod symbolgen;
+mod vm;
+
+use polars::prelude::*;
 
 fn main() {
-    if let Err(err) = cli::execute() {
-        eprintln!("error: {}", err);
-        std::process::exit(1);
-    }
+    let config = match cli::execute() {
+        Ok(c) => c,
+        Err(err) => {
+            eprintln!("error: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    // TODO: move to vm
+    let df = CsvReader::new(config.table_file)
+        .infer_schema(None)
+        .has_header(true)
+        .finish()
+        .unwrap();
+
+    println!("{:?}", df);
 }
