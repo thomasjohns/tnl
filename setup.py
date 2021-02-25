71e2bbb8199500050a105ff1d63466e7249414d2
thomasjohns <thomasjohns@fastmail.com>
2021-02-25 00:40:08 -0800

    add type annotation marker for tnl library

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     name='tnl',
     version='0.1.0',
     packages=['tnl'],
+    package_data={
+        'tnl': ['py.typed']
+    },
     entry_points={
         'console_scripts': [
             'tnl = tnl.__main__:exec_cli'
